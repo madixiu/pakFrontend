46f450a0663c81c888423403905897ac3386dae3
madixiu <mahdi.moradi72@gmail.com>
2025-08-04 10:13:33 +0330

    +custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#3a86ff',
-        'secondary': '#8338ec',
+        'primary': '#2d60a5',
+        'secondary': '#ff5d00',
         'accent': '#ff006e',
-        'custom-blue': {
-          100: '#dbeafe',
-          200: '#bfdbfe',
-          300: '#93c5fd',
-          400: '#60a5fa',
-          500: '#3b82f6',
-          600: '#2563eb',
-          700: '#1d4ed8',
-          800: '#1e40af',
-          900: '#1e3a8a',
-        }
+        'background-1':'#fff',
+        'background-2':'#e7edf6'
+        // 'custom-blue': {
+        //   100: '#dbeafe',
+        //   200: '#bfdbfe',
+        //   300: '#93c5fd',
+        //   400: '#60a5fa',
+        //   500: '#3b82f6',
+        //   600: '#2563eb',
+        //   700: '#1d4ed8',
+        //   800: '#1e40af',
+        //   900: '#1e3a8a',
+        // }
       }
     }
   }
