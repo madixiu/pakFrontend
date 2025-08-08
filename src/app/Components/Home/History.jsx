6c10895edf546eba9a8e0cc7bd521f3ee6258290
madixiu <mahdi.moradi72@gmail.com>
2025-08-08 21:10:42 +0330

    +history home page init

---
+++ src/app/Components/Home/History.jsx
@@ -0,0 +1,13 @@
+function History() {
+  return ( 
+    <div>
+      <div className="flex flex-row mx-2">
+        <span>درباره ما</span>
+        <span>درباره ما</span>
+      </div>
+      <div className="flex">image</div>
+    </div>
+   );
+}
+
+export default History;
